08d08bfeb4dd7fa51f0aea4ef5f4159e5b0489c0
dorryCodes <codesdorry@gmail.com>
2024-11-17 18:40:39 +0300

    getting ready for  css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import './components/Employee';
 import Employee from './components/Employee';
+import { useState } from 'react';
 function App() {
   const showemployees =true;
+ const [task, setTask] =useState('dev');
   return (
     <div className="App">
         {showemployees ?(
           <>
-            <Employee/>
+          <input type='text' onChange={(e)=>{
+            console.log(e.target.value);
+            setTask(e.target.value);
+          }}/>
+            <Employee name ="Dorry" role='intern' />;
+            <Employee name="Caleb" role={task}/>
           </>
          ):(
           <p>You cannot access the employee details</p>
